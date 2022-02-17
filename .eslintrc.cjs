a2a996c5229f826291d3c05953319cb247b17be3
Michael Underwood <mike.underwood@gmail.com>
2022-02-17 11:58:41 -0700

    Install chai-friendly eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    mocha: true,
   },
   extends: [
     'airbnb-base',
@@ -12,4 +13,7 @@ module.exports = {
   },
   rules: {
   },
+  plugins: [
+    'chai-friendly',
+  ],
 };
